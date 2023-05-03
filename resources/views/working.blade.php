c95a9665243f833796014bf363e1d4666688acde
ariel593 <arielferaguirre.2001@gmail.com>
2023-05-03 17:49:12 -0500

    monkey

--- resources/views/working.blade.php
+++ resources/views/working.blade.php
@@ -1,14 +1,13 @@
 @extends('app')
 @section('content')
-<!-- centered image -->
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <h1>Estamos trabajando en este sitio</h1>
-            <img src="{{ asset('img/working.png') }}" alt="" width="350" class="d-inline-block align-text-top">
+    <!-- centered image -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1>Estamos trabajando en este sitio</h1>
+                <iframe src="https://giphy.com/embed/5Zesu5VPNGJlm" width="480" height="270" frameBorder="0"
+                    class="giphy-embed" allowFullScreen></iframe>
+                <p><a href="https://giphy.com/gifs/5Zesu5VPNGJlm">via GIPHY</a></p>
+            </div>
         </div>
-    </div>
-
-
-
-@endsection
+    @endsection
